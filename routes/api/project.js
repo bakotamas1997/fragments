a2052e35bc2156d4df6fdc7c4ac2df4f4638abbe
Tamas Bako <bakotamas1997@gmail.com>
2021-10-28 15:44:26 +0200

    Added route to update project description and name by user, which is part of the project

--- routes/api/project.js
+++ routes/api/project.js
@@ -53,4 +53,22 @@ router.put("/:project_id/:user_id", auth, (req, res) => {
     });
 });
 
+router.put("/:project_id", auth, (req, res) => {
+  User.findById(req.user.id).then((user) => {
+    const selectedProject = user.projects.filter((project) => {
+      return req.params.project_id === project._id.toString();
+    });
+
+    if (selectedProject.length < 0) {
+      return res.status(404).json({ error: "Cannot find project." });
+    }
+
+    Project.findById(req.params.project_id).then((project) => {
+      project.name = req.body.name || project.name;
+      project.description = req.body.description || project.description;
+      project.save().then((project) => res.json(project));
+    });
+  });
+});
+
 module.exports = router;
